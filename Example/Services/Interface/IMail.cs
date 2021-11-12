7da176f722a9c00e1678bf7d82d6f3928ca63ae5
bill huang <billhuang@funday.asia>
2021-11-12 10:53:17 +0800

    新增service資料夾範例

---
+++ Example/Services/Interface/IMail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Services.Interface {
+  public interface IMail {
+    /// <summary>
+    /// 寄信
+    /// </summary>
+    /// <returns></returns>
+    public Task<bool> send();
+  }
+}
---
+++ Example/Services/MailService.cs
@@ -0,0 +1,15 @@
+﻿using Example.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Services {
+  public class MailService: IMail {
+
+    public async Task<bool> send() {
+      //需要加上await才能執行非同步
+      return false;
+    }
+  }
+}
