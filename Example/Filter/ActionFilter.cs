f3fce99fb559c6197803d2e055b5a287b7c366c0
bill huang <billhuang@funday.asia>
2021-11-12 10:55:24 +0800

    新增filter範例

---
+++ Example/Filter/ActionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Filter {
+  public class ActionFilter : Attribute, IActionFilter {
+    public void OnActionExecuted(ActionExecutedContext context) {
+      //context.HttpContext.Response.WriteAsync("Executing AsyncActionFilter. \r\n");
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context) { }
+  }
+  /// <summary>
+  /// 非同步
+  /// </summary>
+  public class AsyncActionFilter : Attribute, IAsyncActionFilter {
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next) {
+      //await context.HttpContext.Response.WriteAsync("Executing AsyncActionFilter.");
+      await next();
+    }
+
+  }
+}
