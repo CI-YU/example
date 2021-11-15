ea31c9faa5ee80c7044c2521647a70d9960396f2
hikaruchiu <hikaruchiu@funday.asia>
2021-11-15 11:59:07 +0800

    StringExcention一些字元處裡的擴充類別 VaildateCoreHelper驗證碼產生圖片

--- Example/Infrastructure/Util/Extentions/StringExtensions.cs
+++ Example/Infrastructure/Util/Extentions/StringExtensions.cs
@@ -140,6 +140,15 @@ namespace Example.Infrastructure.Util.Extentions
 
         #region 其他方法
         /// <summary>
+        /// 指定的字符串是 null 還是 System.String.Empty 字串
+        /// </summary>
+        [DebuggerStepThrough]
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
         /// 判斷字串是否為Null或是空白
         /// </summary>
         /// <param name="s"></param>
@@ -197,6 +206,354 @@ namespace Example.Infrastructure.Util.Extentions
                     return false;
             }
         }
+
+        /// <summary>
+        /// 支援中文長度或全形，中文長度是2
+        /// </summary>
+        /// <param name="value">來源字串</param>
+        /// <returns>回傳字串長度，中文或全形長度是2</returns>
+        public static int TextLength(this string value)
+        {
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            int tempLen = 0;
+            byte[] bytes = ascii.GetBytes(value);
+            foreach(byte b in bytes)
+            {
+                if(b == 63)
+                {
+                    tempLen += 2;
+                }
+                else
+                {
+                    tempLen += 1;
+                }
+            }
+            return tempLen;
+        }
+
+        /// <summary>
+        /// 給URL增加查詢參數
+        /// </summary>
+        /// <param name="url">URL字串</param>
+        /// <param name="queries">要添加的參數如："id=1,cid=2"</param>
+        /// <returns></returns>
+        public static string AddUrlQuery(this string url, params string[] queries)
+        {
+            foreach(string query in queries)
+            {
+                if(!url.Contains("?"))
+                {
+                    url += "?";
+                }
+                else if(!url.EndsWith("&"))
+                {
+                    url += "&";
+                }
+
+                url += query;
+            }
+            return url;
+        }
+
+        /// <summary>
+        /// 取得URL的參數值，沒有的話就回傳空字串
+        /// </summary>
+        public static string GetUrlQuery(this string url, string key)
+        {
+            Uri uri = new Uri(url);
+            string query = uri.Query;
+            if(query.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+            query = query.TrimStart('?');
+            var dict = (from m in query.Split('&', StringSplitOptions.RemoveEmptyEntries)
+                        let strs = m.Split("=")
+                        select new KeyValuePair<string, string>(strs[0], strs[1]))
+                .ToDictionary(m => m.Key, m => m.Value);
+            if(dict.ContainsKey(key))
+            {
+                return dict[key];
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 將字符串轉換byte[]，預設編碼Encoding.UTF8
+        /// </summary>
+        public static byte[] ToBytes(this string value, Encoding encoding = null)
+        {
+            if(encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            return encoding.GetBytes(value);
+        }
+
+        /// <summary>
+        /// 將byte[]轉換為字串，預設編碼Encoding.UTF8
+        /// </summary>
+        public static string ToString(this byte[] bytes, Encoding encoding)
+        {
+            if(encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            return encoding.GetString(bytes);
+        }
+
+        /// <summary>
+        /// 將字串轉換Long型態
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static long ToLong(this string str)
+        {
+            long result;
+            long.TryParse(str, out result);
+            return result;
+        }
+
+        /// <summary>
+        /// 將字串轉換為MD5加密（Intranet 一樣方式）
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToMD5String(this string str)
+        {
+            byte[] b = Encoding.UTF8.GetBytes(str);
+            b = new MD5CryptoServiceProvider().ComputeHash(b);
+            //for (int i = 0; i < b.Length; i++) {
+            //    ret += b[i].ToString("x").PadLeft(2, '0');
+            //}
+
+            //2021-09-27 Hikaru MD5密碼改為與Intranet Web 一樣方式
+            string ret = Convert.ToBase64String(b);
+            return ret;
+        }
+
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="toEncrypt"></param>
+        /// <returns></returns>
+        public static string AesEncrypt(string toEncrypt)
+        {
+            if(string.IsNullOrEmpty(toEncrypt))
+            {
+                return string.Empty;
+            }
+            try
+            {
+                string KeySecret = "gDp4wLdCnCIhbJ12tR2sqH6GTWbURf58"; //與intranet 一樣的Key
+                string KeyIv = "Tvcphq2F2LWeuYhX";
+                byte[] keyArray = Encoding.UTF8.GetBytes(KeySecret);
+                byte[] ivArray = Encoding.UTF8.GetBytes(KeyIv);
+                byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+
+                byte[] useKeyBytes = new byte[16];
+                byte[] useIvBytes = new byte[16];
+
+                if(keyArray.Length > useKeyBytes.Length)
+                {
+                    Array.Copy(keyArray, useKeyBytes, useKeyBytes.Length);
+                }
+                else
+                {
+                    Array.Copy(keyArray, useKeyBytes, keyArray.Length);
+                }
+                if(ivArray.Length > useIvBytes.Length)
+                {
+                    Array.Copy(ivArray, useIvBytes, useIvBytes.Length);
+                }
+                else
+                {
+                    Array.Copy(ivArray, useIvBytes, ivArray.Length);
+                }
+
+                Aes aes = System.Security.Cryptography.Aes.Create();
+                aes.KeySize = 256;//金鑰的大小，位元128,256等
+                aes.BlockSize = 128;//支持的區塊大小
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                aes.Key = useKeyBytes;
+                aes.IV = useIvBytes;//初始化向量，如果没有設定，預設16個0
+
+                ICryptoTransform cTransform = aes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="toDecrypt"></param>
+        /// <returns></returns>
+        public static string AesDecrypt(string toDecrypt)
+        {
+            if(string.IsNullOrEmpty(toDecrypt))
+            {
+                return string.Empty;
+            }
+            try
+            {
+                string KeySecret = "gDp4wLdCnCIhbJ12tR2sqH6GTWbURf58"; //與intranet 一樣的Key
+                string KeyIv = "Tvcphq2F2LWeuYhX";
+                byte[] keyArray = Encoding.UTF8.GetBytes(KeySecret);
+                byte[] ivArray = Encoding.UTF8.GetBytes(KeyIv);
+                byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
+
+                byte[] useKeyBytes = new byte[16];
+                byte[] useIvBytes = new byte[16];
+
+                if(keyArray.Length > useKeyBytes.Length)
+                {
+                    Array.Copy(keyArray, useKeyBytes, useKeyBytes.Length);
+                }
+                else
+                {
+                    Array.Copy(keyArray, useKeyBytes, keyArray.Length);
+                }
+                if(ivArray.Length > useIvBytes.Length)
+                {
+                    Array.Copy(ivArray, useIvBytes, useIvBytes.Length);
+                }
+                else
+                {
+                    Array.Copy(ivArray, useIvBytes, ivArray.Length);
+                }
+
+                Aes aes = System.Security.Cryptography.Aes.Create();
+                aes.KeySize = 256;//金鑰的大小，位元128,256等
+                aes.BlockSize = 128;//支持的區塊大小
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                aes.Key = useKeyBytes;
+                aes.IV = useIvBytes;//初始化向量，如果没有設定，預設16個0
+
+
+                ICryptoTransform cTransform = aes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch(Exception)
+            {                
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// string轉int
+        /// </summary>
+        /// <param name="str">字串</param>
+        /// <returns></returns>
+        public static int ToInt(this string str)
+        {
+            str = str.Replace("\0", "");
+            if(string.IsNullOrEmpty(str))
+                return 0;
+            return Convert.ToInt32(str);
+        }
+
+        /// <summary>
+        /// 將Json字串反序列化為物件
+        /// </summary>
+        /// <typeparam name="T">物件類型</typeparam>
+        /// <param name="jsonStr">Json字串</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string jsonStr)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonStr);
+        }
+
+        /// <summary>
+        /// 將Json字串反序列化為物件
+        /// </summary>
+        /// <param name="jsonStr">Json字串</param>
+        /// <param name="type">物件類型</param>
+        /// <returns></returns>
+        public static object ToObject(this string jsonStr, Type type)
+        {
+            return JsonConvert.DeserializeObject(jsonStr, type);
+        }
+
+        /// <summary>
+        /// 將XML字串反序列化為物件
+        /// </summary>
+        /// <typeparam name="T">物件類型</typeparam>
+        /// <param name="xmlStr">XML字串</param>
+        /// <returns></returns>
+        public static T XmlStrToObject<T>(this string xmlStr)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlStr);
+            string jsonJsonStr = JsonConvert.SerializeXmlNode(doc);
+
+            return JsonConvert.DeserializeObject<T>(jsonJsonStr);
+        }
+
+        /// <summary>
+        /// 將XML字串反序列化為JObject
+        /// </summary>
+        /// <param name="xmlStr">XML字符串</param>
+        /// <returns></returns>
+        public static JObject XmlStrToJObject(this string xmlStr)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlStr);
+            string jsonJsonStr = JsonConvert.SerializeXmlNode(doc);
+
+            return JsonConvert.DeserializeObject<JObject>(jsonJsonStr);
+        }
+
+        /// <summary>
+        /// 將Json字串轉換為 List'T'
+        /// </summary>
+        /// <typeparam name="T">物件類型</typeparam>
+        /// <param name="jsonStr">Json字串</param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this string jsonStr)
+        {
+            return string.IsNullOrEmpty(jsonStr) ? null : JsonConvert.DeserializeObject<List<T>>(jsonStr);
+        }
+        /// <summary>
+        /// 將Json字串轉換DataTable
+        /// </summary>
+        /// <param name="jsonStr">Json字串</param>
+        /// <returns></returns>
+        public static DataTable ToDataTable(this string jsonStr)
+        {
+            return jsonStr == null ? null : JsonConvert.DeserializeObject<DataTable>(jsonStr);
+        }
+
+        /// <summary>
+        /// 將将Json字串轉為JObject
+        /// </summary>
+        /// <param name="jsonStr">Json字串</param>
+        /// <returns></returns>
+        public static JObject ToJObject(this string jsonStr)
+        {
+            return jsonStr == null ? JObject.Parse("{}") : JObject.Parse(jsonStr.Replace("&nbsp;", ""));
+        }
+
+        /// <summary>
+        /// 將Json字串轉為JArray
+        /// </summary>
+        /// <param name="jsonStr">Json字串</param>
+        /// <returns></returns>
+        public static JArray ToJArray(this string jsonStr)
+        {
+            return jsonStr == null ? JArray.Parse("[]") : JArray.Parse(jsonStr.Replace("&nbsp;", ""));
+        }
+
+
         #endregion
     }
 }
---
+++ Example/Infrastructure/Util/Helper/ValidateCodeHelper.cs
@@ -0,0 +1,96 @@
+﻿using shortid;
+using shortid.Configuration;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace Example.Infrastructure.Util.Helper
+{
+    public class ValidateCodeHelper
+    {
+        /// <summary>
+        /// 隨機產生 n 長度不重複文字碼
+        /// </summary>
+        /// <returns></returns>
+        public static string CreateUniqueId(int genLength)
+        {
+            GenerationOptions options = new GenerationOptions
+            {
+                UseSpecialCharacters = false,
+                Length = genLength //長度
+            };
+            string uniqueId = ShortId.Generate(options);
+
+            return uniqueId;
+        }
+
+        /// <summary>
+        /// 生出驗證碼圖片
+        /// </summary>
+        /// <param name="validateCode"></param>
+        /// <returns></returns>
+        public static byte[] CreateValidateGraphic(string validateCode)
+        {
+            Bitmap image = new Bitmap((int)Math.Ceiling(validateCode.Length * 16.0), 27);
+            Graphics g = Graphics.FromImage(image);
+            try
+            {
+                //隨機產生
+                Random random = new Random();
+                //清空圖片背景色
+                g.Clear(Color.White);
+                //繪製圖片干擾線
+                for(int i = 0; i < 25; i++)
+                {
+                    int x1 = random.Next(image.Width);
+                    int x2 = random.Next(image.Width);
+                    int y1 = random.Next(image.Height);
+                    int y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, x2, y1, y2);
+                }
+                Font font = new Font("Arial", 13, (FontStyle.Bold | FontStyle.Italic));
+                LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height), Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(validateCode, font, brush, 3, 2);
+
+                //繪製圖片前景干擾線
+                for(int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
+                }
+                //繪製圖片邊框
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+
+                //保存圖片資料
+                MemoryStream stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
+
+                //輸出
+                return stream.ToArray();
+            }
+            finally
+            {
+                g.Dispose();
+                image.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 產生驗證碼圖片，傳回圖片的Base64字串
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="length">驗證碼長度</param>
+        /// <returns></returns>
+        public static string CreateBase64String(out string code, int length = 4)
+        {
+            code = CreateUniqueId(length);
+            var bytes = CreateValidateGraphic(code);
+            return "data:image/png;base64," + Convert.ToBase64String(bytes);
+        }
+
+    }
+}
